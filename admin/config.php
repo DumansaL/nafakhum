58e188b350179242b0cac8e0a6bdacd79cb256b8
Nahid <Nahid@192.168.2.100>
2013-07-31 23:53:22 +0600

    Styled the menu

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/nafakhum/admin/');
+define('HTTP_CATALOG', 'http://localhost/nafakhum/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/nafakhum/admin/');
+define('HTTPS_CATALOG', 'http://localhost/nafakhum/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\wamp\www\nafakhum/admin/');
+define('DIR_SYSTEM', 'E:\wamp\www\nafakhum/system/');
+define('DIR_DATABASE', 'E:\wamp\www\nafakhum/system/database/');
+define('DIR_LANGUAGE', 'E:\wamp\www\nafakhum/admin/language/');
+define('DIR_TEMPLATE', 'E:\wamp\www\nafakhum/admin/view/template/');
+define('DIR_CONFIG', 'E:\wamp\www\nafakhum/system/config/');
+define('DIR_IMAGE', 'E:\wamp\www\nafakhum/image/');
+define('DIR_CACHE', 'E:\wamp\www\nafakhum/system/cache/');
+define('DIR_DOWNLOAD', 'E:\wamp\www\nafakhum/download/');
+define('DIR_LOGS', 'E:\wamp\www\nafakhum/system/logs/');
+define('DIR_CATALOG', 'E:\wamp\www\nafakhum/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'nafakhum');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/nafakhum/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/nafakhum/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\wamp\www\nafakhum/catalog/');
+define('DIR_SYSTEM', 'E:\wamp\www\nafakhum/system/');
+define('DIR_DATABASE', 'E:\wamp\www\nafakhum/system/database/');
+define('DIR_LANGUAGE', 'E:\wamp\www\nafakhum/catalog/language/');
+define('DIR_TEMPLATE', 'E:\wamp\www\nafakhum/catalog/view/theme/');
+define('DIR_CONFIG', 'E:\wamp\www\nafakhum/system/config/');
+define('DIR_IMAGE', 'E:\wamp\www\nafakhum/image/');
+define('DIR_CACHE', 'E:\wamp\www\nafakhum/system/cache/');
+define('DIR_DOWNLOAD', 'E:\wamp\www\nafakhum/download/');
+define('DIR_LOGS', 'E:\wamp\www\nafakhum/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'nafakhum');
+define('DB_PREFIX', 'oc_');
+?>
